3ffe272cea664ee546536dcfc30e09121eec172d
SuzukiEma-andfactory <suzuki_ema@andfactory.co.jp>
2025-01-04 14:05:43 +0900

    next.configでリダイレクト実装

--- next-tutrial/next.config.mjs
+++ next-tutrial/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        // リダイレクトが発生するパス
+        source: '/about',
+        // リダイレクト先のパス
+        destination: '/',
+        // リダイレクトが永続的ならtrue（リダイレクトを永久にキャッシュされる）
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
