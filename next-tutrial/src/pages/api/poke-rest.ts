70986c66452b0ef94f0fee2b5abc89fe94526e48
SuzukiEma-andfactory <suzuki_ema@andfactory.co.jp>
2025-01-10 23:11:03 +0900

    名前を日本語表示にする

--- next-tutrial/src/pages/api/poke-rest.ts
+++ next-tutrial/src/pages/api/poke-rest.ts
@@ -11,20 +11,28 @@ export const usePokemonData = () => {
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
+        // ポケモン一覧取得
         const response = await fetch(
           `https://pokeapi.co/api/v2/pokemon?limit=10`
         );
         const data = await response.json();
 
-        // 一覧に表示する要素
+        // ポケモン一覧で表示する要素を取得
         const pokemonDetails = await Promise.all(
           data.results.map(async (pokemon: { name: string; url: string }) => {
             const res = await fetch(pokemon.url);
             // ポケモンのタイプ、重さ、能力の情報
             const details = await res.json();
+            // 日本語の名前を取得するのに必要
+            // fetch関数、json関数はPromise（非同期処理の進行状況を管理するオブジェクト）を返す
+            // 非同期処理の完了前、namesにアクセス不可なので処理の完了待機する（await）必要がある
+            const speciesRes = await fetch(details.species.url).then((res) =>
+              res.json()
+            );
 
             return {
-              name: pokemon.name,
+              // 日本語の名前を取得
+              name: speciesRes.names[0].name,
               image:
                 details.sprites.other['official-artwork'].front_default ||
                 // ドット絵
