b789451f5780d6ff51353eab7f4349b9eb9e95b3
SuzukiEma-andfactory <suzuki_ema@andfactory.co.jp>
2025-01-11 17:49:02 +0900

    詳細ページでポケモンデータを表示

--- next-tutrial/src/pages/hooks/rest/usePokemonDetails.ts
+++ next-tutrial/src/pages/hooks/rest/usePokemonDetails.ts
@@ -1,51 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export type PokemonDetail = {
   name: string;
-  image: string;
-  types: string[];
-  weight: number;
-  abilities: string[];
+  image?: string;
+  encount: string;
+  type: string[];
+  capture_rate: number;
+  abilities?: string[];
 };
 
 export const usePokemonDetails = (url: string | null) => {
   const [pokemonDetail, setPokemonDetail] = useState<PokemonDetail | null>(
     null
   );
+  const [apiUrl, setApiUrl] = useState<string | null>(url);
 
   const fetchPokemonDetails = async (url: string) => {
     try {
       const res = await fetch(url);
       const details = await res.json();
-      console.log('🟢', details);
-
-      const speciesRes = await fetch(details.species.url).then((res) =>
-        res.json()
-      );
 
       const pokemonDetail = {
-        name: speciesRes.names[0].name,
-        image:
-          details.sprites.other['official-artwork'].front_default ||
-          details.sprites.front_default,
-        types: details.types.map((type: any) => type.type.name),
-        weight: details.weight / 10, // 重さをkgに変換
-        abilities: details.abilities.map(
-          (ability: any) => ability.ability.name
-        ),
+        name: details.names[0].name,
+        type: details.genera[0].genus,
+        encount: details.pal_park_encounters[0].rate,
+        capture_rate: details.capture_rate,
       };
 
       setPokemonDetail(pokemonDetail);
+      console.log('🟣', pokemonDetail);
     } catch (err) {
       console.error('ポケモンデータの取得に失敗しました:', err);
     }
   };
 
-  if (url) {
-    fetchPokemonDetails(url);
-  }
+  useEffect(() => {
+    if (apiUrl) {
+      fetchPokemonDetails(apiUrl);
+    }
+  }, [apiUrl]);
 
-  return { pokemonDetail };
+  return { pokemonDetail, setApiUrl };
 };
 
 export default usePokemonDetails;
--- next-tutrial/src/pages/pokemon/[name]/component.tsx
+++ next-tutrial/src/pages/pokemon/[name]/component.tsx
@@ -1,8 +1,24 @@
+import { usePokemonContext } from '@/context/PokemonContext';
+import usePokemonDetails from '@/pages/hooks/rest/usePokemonDetails';
+
 const Component = () => {
+  const { selectedPokemonImage, selectedPokemonUrl } = usePokemonContext();
+  const { pokemonDetail } = usePokemonDetails(selectedPokemonUrl);
+  const imageSize = 240;
+
   return (
     <>
       <div>
-        <p>s</p>
+        <p>{pokemonDetail?.name}</p>
+        <img
+          src={selectedPokemonImage || ''}
+          alt=''
+          width={imageSize}
+          height={imageSize}
+        />
+        <p>タイプ：{pokemonDetail?.type}</p>
+        <p>遭遇率：{pokemonDetail?.encount}%</p>
+        <p>捕獲率：{pokemonDetail?.capture_rate}%</p>
       </div>
     </>
   );
