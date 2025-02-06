876ea3c5f89bf6d04425e104dc8bea664f02c4a4
SuzukiEma-andfactory <suzuki_ema@andfactory.co.jp>
2025-02-06 18:02:52 +0900

    next/script

--- next-tutrial/src/pages/index.tsx
+++ next-tutrial/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from '@/components/Layout';
 import List from '@/components/List';
 import usePokemonIndex from '@/hooks/graphql/usePokemonIndex';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { styled } from 'styled-components';
 
 // const geistSans = localFont({
@@ -32,6 +33,26 @@ export default function Home() {
   const { pokemonList } = usePokemonIndex();
 
   return (
+    <>
+      <Layout>
+        <StyledContainer>
+          <List items={pokemonList}></List>
+        </StyledContainer>
+      </Layout>
+
+      {/* スクリプトの読み込みタイミングを指定できる */}
+      <Script
+        // ロードするスクリプトのURL（ここではFacebook SDKのURLを使う）
+        src='https://connect.facebook.net/en_US/sdk.js'
+        // ページの読み込み完了後にスクリプトをロード
+        strategy='lazyOnload'
+        // スクリプトが正しくロードされたら実行
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+    </>
+
     // <div
     //   className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     // >
@@ -51,10 +72,5 @@ export default function Home() {
     //     {/* <button onClick={handleUrlUpdate}>画面遷移なしでURL更新</button> */}
     //   </main>
     // </div>
-    <Layout>
-      <StyledContainer>
-        <List items={pokemonList}></List>
-      </StyledContainer>
-    </Layout>
   );
 }
